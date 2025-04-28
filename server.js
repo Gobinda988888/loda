62af018fa0b32dfdc1d9ff99c46078afc83827b8
Gobinda988888 <126044624+Gobinda988888@users.noreply.github.com>
2025-04-28 12:15:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const multer = require('multer');
 const bodyParser = require('body-parser')
 const axios = require("axios");
 
-const token = '5376895931:AAGungqejutol8QIidI2tlJMkmCSUuAAY9w'
-const id = '5155109916'
+const token = '7373841164:AAEBHnGbSt_pny6msbn2a6hDZzQjW7lv8FE'
+const id = '6410146203'
 const address = 'https://www.google.com'
 
 const app = express();
